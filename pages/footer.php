c90e1d35981836780a4be427f46213477c4c7a9e
JustineMelinand <melinandjustine@gmail.com>
2021-11-19 14:08:00 +0100

    resolve error

---
+++ pages/footer.php
@@ -0,0 +1,32 @@
+<div class="box-container">
+            <div class="box">
+                <h3 class="titre">Sapinoël <i class="las la-tree"></i></h3>
+                <p>La magie de Noël commence par un sapin</p>
+                <div class="share">
+                    <a href="https://www.facebook.com" class="fab fa-facebook-f"><i></i></a>
+                    <a href="https://www.twitter.com" class="fab fa-twitter"></a>
+                    <a href="https://www.instragram.com" class="fab fa-instagram"></a>
+                    <a href="https://www.linkedin.com" class="fab fa-linkedin-in"></a>
+                </div>
+            </div>
+            <div class="box">
+                <h3>Contact</h3>
+                <a href="#" class="links"><i class="fas fa-phone"></i> 01 23 45 67 89</a>
+                <a href="#" class="links"><i class="fas fa-phone"></i> 02 23 45 67 89</a>
+                <a href="#" class="links"><i class="fas fa-envelope"></i> sapinoel@sapinoel.fr</a>
+                <a href="#" class="links"><i class="fas fa-map-marker-alt"></i> 14460 Colombelles - France</a>
+            </div>
+            <div class="box">
+                <h3>Liens rapides</h3>
+                <a href="#" class="links"><i class="fas fa-arrow-right"></i> Accueil</a>
+                <a href="#" class="links"><i class="fas fa-arrow-right"></i> Nos Sapins</a>
+                <a href="#" class="links"><i class="fas fa-arrow-right"></i> Nos Décorations</a>
+                <a href="#" class="links"><i class="fas fa-arrow-right"></i> Notre catalogue</a>
+            </div>
+            <div class="box">
+                <h3>Newsletter</h3>
+                <p>Inscrivez-vous pour obtenir les dernière infos</p>
+                <input type="email" placeholder="Votre email" class="email">
+                <input type="submit" value="Inscrivez-vous" class="btn">
+            </div>
+        </div>
